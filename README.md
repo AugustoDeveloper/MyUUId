ed3066301774dcbb1a6f0c520f2a0bef3d9f7ec5
Augusto <dev.augusto@gmail.com>
2022-04-16 14:46:02 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# MyUUId
+
+É o projeto para estudar como é o processo da geração das UUId e suas versões.
+
+## Goals
+- [ ] Desenvolver estrutura UUId que permita interpretar cada uma de suas versões 
+    - [ ] Desenvolver a versão 1
+        - [x] Implementar criação de time_low
+        - [x] Implementar criação de time_mid
+        - [x] Implementar criação de time_hi_and_version
+        - [x] Implementar criação de clock_seq_low
+        - [x] Implementar criação de time_seq_hi_and_reserved
+        - [x] Implementar criação de node
+        - [ ] Implementar forma simples de interpretar UUId a partir de string.
+    - [ ] Desenvolver a versão 2
+    - [ ] Desenvolver a versão 3
+    - [ ] Desenvolver a versão 4
+    - [ ] Desenvolver a versão 5
+
+
+### References
+- [A Universally Unique IDentifier - RFC 4122](https://datatracker.ietf.org/doc/html/rfc4122#section-4.2.1)
---
+++ src/my-uuid/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net.NetworkInformation;
+
+var myId = MyUUId.NewUUId();
+Console.WriteLine(myId);
+Console.WriteLine(Guid.Parse(myId.ToString()));
+Console.WriteLine(Guid.NewGuid());
+
+public readonly struct MyUUId
+{
+    private readonly uint timeLow;
+    private readonly ushort timeMid;
+    private readonly ushort timeHiAndVersion;
+    private readonly byte clockSeqHiAndReserved;
+    private readonly byte clockSeqLow;
+    private readonly byte[] node;
+
+    public static readonly DateTime StartTimestamp = new DateTime(1582, 10, 15, 0, 0, 0, 0, DateTimeKind.Utc);
+
+    public MyUUId(
+            uint timeLow, 
+            ushort timeMid, 
+            ushort timeHiAndVersion,
+            byte clockSeqHiAndReserved,
+            byte clockSeqLow,
+            byte[] node)
+    {
+        this.timeLow = timeLow;
+        this.timeMid = timeMid;
+        this.timeHiAndVersion = timeHiAndVersion;
+        this.clockSeqHiAndReserved = clockSeqHiAndReserved;
+        this.clockSeqLow = clockSeqLow;
+        this.node = node;
+    }
+
+    public static MyUUId NewUUId()
+    {
+        var clock = DateTime.UtcNow.Ticks;
+        var currentTimestamp = (long)(clock - MyUUId.StartTimestamp.Ticks) * 1000 + 1024;
+
+        var timeLow = (uint)(currentTimestamp & 0xFFFFFFFF);
+        var timeMid = (ushort)((currentTimestamp & 0xFFFF00000000) >> 32);
+        var timeHiAndVersion = (ushort)(currentTimestamp >> 48);
+        ushort version = 1 << 12;
+        timeHiAndVersion = (ushort) (version | timeHiAndVersion);
+        var clockSeqLow = (byte) (clock & 0xFF);
+        var clockSeqHiAndReserved = (byte) (((clock & 0x3F00) >> 8) | 0x80);
+        
+        var x = NetworkInterface.GetAllNetworkInterfaces();
+        var address = x[0].GetPhysicalAddress();
+
+        return new(timeLow, timeMid, timeHiAndVersion, clockSeqLow, clockSeqHiAndReserved, address.GetAddressBytes());
+    }
+
+    public override string ToString()
+        => timeLow.ToString("x8") + "-" +
+            timeMid.ToString("x4") + "-" +
+            timeHiAndVersion.ToString("x4") + "-" +
+            clockSeqLow.ToString("x2") +
+            clockSeqHiAndReserved.ToString("x2") + "-" +
+            Convert.ToHexString(node).ToLower();
+}
+
+/*
+  uuid->clock_seq_low = clock_seq & 0xFF;
+  uuid->clock_seq_hi_and_reserved = (clock_seq & 0x3F00) >> 8;
+  uuid->clock_seq_hi_and_reserved |= 0x80;
+
+ o  Set the clock_seq_low field to the eight least significant bits
+      (bits zero through 7) of the clock sequence in the same order of
+      significance.
+
+   o  Set the 6 least significant bits (bits zero through 5) of the
+      clock_seq_hi_and_reserved field to the 6 most significant bits
+      (bits 8 through 13) of the clock sequence in the same order of
+      significance.
+
+   o  Set the two most significant bits (bits 6 and 7) of the
+      clock_seq_hi_and_reserved to zero and one, respectively.
+
+*/
